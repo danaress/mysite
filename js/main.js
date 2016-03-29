9e127380ca28327149541a13fe240f6957fd6ab8
danaress <ressdana@gmail.com>
2016-03-29 15:05:04 -0600

    added hover boxes and controller

---
+++ js/main.js
@@ -0,0 +1,11 @@
+angular.module('myApp', [])
+
+angular.module('myApp')
+  .controller('mainController', ['$scope', function($scope){
+
+$scope.test = function(){
+  console.log("working")
+}
+
+
+    }]);
