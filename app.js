fa94502effae3dfcd5ad107bd33780820e8a7239
danaress <ressdana@gmail.com>
2016-04-03 13:26:39 -0600

    added contact form, started with responsive grid

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-// var express = require('express');
+var express = require('express');
+var nodemailer = require("nodemailer");
+var app = express();
 
-// var app = express();
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "danaresstest",
+        pass: "0okmkjtest"
+    }
+});
 
-// app.use(express.static(__dirname + '/public'));
-// app.get('/', function(req, res){
-//   res.sendFile('index.html', {root : './public'});
-// });
 
 
-// var port = 3000
-// app.listen(port, function(){
-//   console.log('Server running on port ' + port);
+app.use(express.static(__dirname + '/public'));
 
-// })
+app.get('/',function(req,res){
+res.sendfile('public/index.html');
+});
+
+app.get('/send',function(req,res){
+var mailOptions={
+   to : 'ressdana@gmail.com',
+   subject : req.query.subject,
+   text : req.query.text
+}
+console.log(mailOptions);
+smtpTransport.sendMail(mailOptions, function(error, response){
+if(error){
+console.log(error);
+res.end("error");
+}else{
+console.log("Message sent: " + response.message);
+res.end("sent");
+}
+});
+});
+
+var port = 3000
+app.listen(port, function(){
+
+})
