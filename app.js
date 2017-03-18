1bb6684d165fc53e2397a27b5098f3851ac8d120
Dana Ress <danaress@MacBook-Air.local>
2017-03-18 11:50:38 -0600

    1 page

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 var express = require('express');
-var nodemailer = require("nodemailer");
 var app = express();
-
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-
-    }
-});
-
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
 res.sendfile('public/index.html');
-});
-
-app.get('/send',function(req,res){
-var mailOptions={
-   to : 'ressdana@gmail.com',
-   subject : req.query.subject,
-   text : req.query.text
-}
-console.log(mailOptions);
-smtpTransport.sendMail(mailOptions, function(error, response){
-if(error){
-console.log(error);
-res.end("error");
-}else{
-console.log("Message sent: " + response.message);
-res.end("sent");
-}
-});
 });
 
 var port = 80
